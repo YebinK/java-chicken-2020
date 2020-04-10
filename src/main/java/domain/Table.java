17a1b5608e680a1243b0343cc2d529d0e4d2308b
yebink <yebin825@gmail.com>
2020-04-10 13:57:09 +0900

    feat: 한 메뉴 당 최대 99개 주문 기능 구현

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,14 +1,41 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
+    private static final int MAX_MENU_SIZE = 99;
+
     private final int number;
+    private final Map<Menu, Integer> menus;
 
     public Table(final int number) {
         this.number = number;
+        this.menus = new HashMap<>();
     }
 
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    public void add(Menu menu, int menuSize) {
+        if (!isOrderable(menu, menuSize)) {
+            throw new IllegalArgumentException("한 메뉴 당 최대 99개 주문 가능합니다.");
+        }
+        this.menus.put(menu, menuSize);
+    }
+
+    public boolean isOrderable(Menu menu, int menuSize) {
+        return !(menus.containsKey(menu) && menus.get(menu) + menuSize > MAX_MENU_SIZE);
+    }
+
+    public double calculate() {
+        int payAmount = 0;
+        for (Map.Entry<Menu, Integer> entry : menus.entrySet()) {
+            payAmount = entry.getKey().getPrice() * entry.getValue();
+        }
+
+        return payAmount;
     }
 }
--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -1,5 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TableTest {
 
+    @DisplayName("테이블에 메뉴 추가")
+    @Test
+    void addMenu() {
+        Table table = TableRepository.tables().get(1);
+        Menu menu = MenuRepository.menus().get(1);
+        table.add(menu, 1);
+
+        assertThat(table.calculate()).isEqualTo(16_000);
+    }
+
+    @DisplayName("테이블에 메뉴 99개 이상 주문 시 예외 발생")
+    @Test
+    void addMenuOver99() {
+        Table table = TableRepository.tables().get(1);
+        Menu menu = MenuRepository.menus().get(1);
+
+        assertThatThrownBy(() -> table.add(menu, 100))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
